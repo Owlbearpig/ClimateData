2811e04c9cf29f10967ae821d35b2e2b47c030c3
Owlbearpig <alexj07@gmail.com>
2021-10-30 16:20:17 +0200

    -v01

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class TDSDataPoint:
         self.filepath = filepath
         self.date = self.getdate()
         self.data = self.parse_data()
+        self.relative_time = None
 
     def __repr__(self):
         return repr(str(self.date))
@@ -28,21 +29,24 @@ class TDSDataPoint:
 
     def parse_data(self):
         df = pd.read_csv(self.filepath, sep='	', skiprows=3)
+        data = np.array(df)  # same arrays as taipan. t = data[:, 0], y = data[:, 1]
+        # can add preprocessing function here. Offset correction, bandpass filter, ...
 
-        return np.array(df)
+        return data
 
 
 class ClimateLogEntry:
     # parses climate data, stores as one object
     def __init__(self, file_line):
-        self.date, self.temp, self.rh, self.ah, self.idx = self.parse_line(file_line)
+        self.file_line = file_line
+        self.date, self.temp, self.rh, self.ah, self.idx = self.parse_line()
 
     def __repr__(self):
         return repr(f'Date: {str(self.date)}, Temp: {self.temp} deg, RH: {self.rh}%, AH: {self.ah}%, index: {self.idx}')
 
-    def parse_line(self, file_line):
+    def parse_line(self):
         # splits line into useful parts
-        l = str(file_line)
+        l = str(self.file_line)
         l = l.replace('         ', '')
         l_split = l.split(',')
 
@@ -61,6 +65,10 @@ class DataPoint:
         self.tds_data = tds_data_point
         self.climate_data = climate_log_entry
         self.measurement_type = ''
+        self.nearest_ref_dp = None
+
+    def __repr__(self):
+        return repr(str(self.tds_data.date))
 
 
 def parse_tds_data_files(data_dir):
@@ -133,7 +141,25 @@ def determine_measurement_type(data_points):
             data_point.measurement_type = 'Reference'
 
 
-if __name__ == '__main__':
+def add_relative_time(data_points):
+    sorted_data_points = sorted(data_points, key=lambda dp: dp.tds_data.date)
+    for dp in sorted_data_points:
+        dp.tds_data.relative_time = dp.tds_data.date - sorted_data_points[0].tds_data.date
+
+    return sorted_data_points
+
+
+def add_ref_to_samples(data_points):
+    ref_list = [dp for dp in data_points if dp.measurement_type == 'Reference']
+    for dp in data_points:
+        if dp.measurement_type == 'Reference':
+            dp.nearest_ref_dp = dp
+        else:
+            ref_time_diffs = np.array([(dp.tds_data.date-ref_dp.tds_data.date).total_seconds() for ref_dp in ref_list])
+            dp.nearest_ref_dp = ref_list[np.argmin(np.abs(ref_time_diffs))]
+
+
+def main():
     data_dir = PROJECT_DIR / 'Data'
 
     tds_data_points = parse_tds_data_files(data_dir)
@@ -144,9 +170,48 @@ if __name__ == '__main__':
 
     determine_measurement_type(matched_data_points)
 
-    rh = [dp.climate_data.rh for dp in matched_data_points]
+    add_ref_to_samples(matched_data_points)
 
-    plt.plot(rh)
+    return add_relative_time(matched_data_points)
+
+
+if __name__ == '__main__':
+    """
+    Missing offset correction and frequency filter, but shouldn't be too hard to add (look in TDSDataPoint).
+    
+    data_dir: top level data(rpi climate, Menlo data) directory
+    
+    What this(main()) should do:
+    1. Find all thz data files and parse each file (parse_tds_data_files)
+    
+    2. Find all climate data files and parse each line of each file 
+    into a ClimateLogEntry object thingy (parse_humidity_files)
+    
+    3. Go through each thz data file and find the nearest climate log entry (wrt. time) 
+    and combine each into a single DataPoint (match_files)
+    
+    4. Add an attribute to each DataPoint which says 
+    if it's a Reference, Sample or Invalid(later ones look weird : ) ) measurement (determine_measurement_type)
+    
+    5. Go through all the DataPoints again and find the nearest reference DataPoint
+    then add it as an attribute (add_ref_to_samples)
+    
+    6. Eh, last step is probably not really needed, it just sorts the points and adds an attribute 
+    showing the time difference to the very first measurement (add_relative_time)
+    
+    """
+    sorted_data_points = main()
+
+    for dp in sorted_data_points:
+        print(dp, dp.measurement_type)
+        print(dp.climate_data, '\n')
+        thz_data = dp.tds_data.data
+        """
+        plt.plot(thz_data[:, 0], thz_data[:, 1])
+        plt.show()
+        """
+
+    temp = [dp.climate_data.temp for dp in sorted_data_points]
+
+    plt.plot(temp)
     plt.show()
-
-    # can we interpolate climate data ???
